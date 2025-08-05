b11337330114bfc7ed31c52c34de4ea74b253468
Brian Thorne <bthornemail@gmail.com>
2025-08-05 12:13:53 -0700

    Moving to UniversalLifeProtiocl temporarty

--- scripts/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export NVM_DIR="$HOME/.nvm"
-# Load nvm
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-# Now run your Node app with npm start or whatever you want
-cd /home/main/github/singularity2d
-npm run dev
